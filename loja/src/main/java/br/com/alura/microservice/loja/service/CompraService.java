7f2389501838340dd79b15e15e5823a1d3e718a8
Jefferson <jeffrm@gmail.com>
2022-10-11 11:26:34 -0300

    2.4 - Implementando o serviço fornecedor

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -7,10 +7,12 @@ import org.springframework.web.client.RestTemplate;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 
+@Service
 public class CompraService {
 
 	public void realizaCompra(CompraDTO compra) {
 
+		//loja fazendo requisição para localizar o endereço do fornecedor
 		RestTemplate cliente = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange 
 			= cliente.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado()
